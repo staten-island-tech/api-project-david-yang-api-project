f9c85e7957a337452c2d64ba15ee5ad1939d36a2
Icebrave <71671391+Icebrave@users.noreply.github.com>
2021-05-26 20:51:06 -0400

    .

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,16 @@ import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
 const key = "YOURKEYHERE";
+console.log("connected");
+
+const query = async function () {
+  try {
+    const response = await fetch("http://numbersapi.com/1..50");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    alert("Something went wrong");
+  }
+};
+query();
