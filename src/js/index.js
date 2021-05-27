1481ada77d63268df4c377979ae6d10a50a224bb
Icebrave <71671391+Icebrave@users.noreply.github.com>
2021-05-27 00:38:04 -0400

    .

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,38 @@ const query = async function () {
     const response = await fetch("http://numbersapi.com/1..50");
     const data = await response.json();
     console.log(data);
+    data.results.array.forEach((element) => {
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `<div class="movie-card">
+      <div class="movie-card-front">
+        <img
+          src="https://image.tmdb.org/t/p/w300/r7vmZjiyZw9rpJMQJdXpjgiCOk9.jpg"
+          alt=""
+          class="poster"
+        />
+      </div>
+      <div class="movie-card-back">
+        <h3 class="movie-card-header">The Dark Knight</h3>
+        <div class="score-box">
+          <p class="user-score">Community Score</p>
+          <p class="user-score">8.4</p>
+        </div>
+
+        <div class="release-box">
+          <p class="release-date">Released</p>
+          <p class="release-date">2020-06-12</p>
+        </div>
+
+        <div class="movie-genres">
+          <li class="movie-genre">Sci-Fi</li>
+          <li class="movie-genre">Fantasy</li>
+          <li class="movie-genre">Horror</li>
+        </div>
+      </div>
+    </div>`
+      );
+    });
   } catch (error) {
     console.log(error);
     alert("Something went wrong");
